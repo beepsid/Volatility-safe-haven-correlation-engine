39000a91760af4444b10b1c85e63c576824895b7
ΛYUSH <siddhantsinghchauhan1@gmail.com>
2026-05-03 20:23:01 +0530

    view gold crash

--- sql/view_gold_crash.sql
+++ sql/view_gold_crash.sql
@@ -5,6 +5,6 @@ date_trunc('week', trade_date) as week,
 avg(gold_price) as gold_price_on_crash,
 count(*) as crash_days
 from market_data
-where high_vol_flag='True'
+where high_vol_flag=True
 group by week
 order by week;
