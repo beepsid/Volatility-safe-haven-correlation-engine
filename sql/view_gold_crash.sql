19ba8d50ca5612dec8113fb932ba5a1e5e8d4abb
ΛYUSH <siddhantsinghchauhan1@gmail.com>
2026-05-03 23:35:55 +0530

    gold crash update

--- sql/view_gold_crash.sql
+++ sql/view_gold_crash.sql
@@ -1,8 +1,8 @@
-create view vw_gold_price_during_crash as
+create view vw_gold_return_during_crash as
 
 select 
 date_trunc('week', trade_date) as week,
-avg(gold_price) as gold_price_on_crash,
+avg(gold_return) as gold_return_on_crash,
 count(*) as crash_days
 from market_data
 where high_vol_flag=True
